8597fdd8bb480b555cded6024e5bad080a35cb3b
Neulovim <45709081+Neulovim@users.noreply.github.com>
2021-05-22 06:08:35 -0700

    change types in article model

--- MostPopularArticles/Source/Model/Article.swift
+++ MostPopularArticles/Source/Model/Article.swift
@@ -25,12 +25,12 @@ struct Result: Codable {
     let uri: String
     let url: String
     let id, assetID: Int
-    let source: Source
+    let source: String
     let publishedDate, updated, section, subsection: String
     let nytdsection, adxKeywords: String
     let column: JSONNull?
     let byline: String
-    let type: ResultType
+    let type: String
     let title, abstract: String
     let desFacet, orgFacet, perFacet, geoFacet: [String]
     let media: [Media]
@@ -55,8 +55,8 @@ struct Result: Codable {
 
 // MARK: - Media
 struct Media: Codable {
-    let type: MediaType
-    let subtype: Subtype
+    let type: String
+    let subtype: String
     let caption, copyright: String
     let approvedForSyndication: Int
     let mediaMetadata: [MediaMetadatum]
@@ -71,32 +71,8 @@ struct Media: Codable {
 // MARK: - MediaMetadatum
 struct MediaMetadatum: Codable {
     let url: String
-    let format: Format
+    let format: String
     let height, width: Int
-}
-
-enum Format: String, Codable {
-    case mediumThreeByTwo210 = "mediumThreeByTwo210"
-    case mediumThreeByTwo440 = "mediumThreeByTwo440"
-    case standardThumbnail = "Standard Thumbnail"
-}
-
-enum Subtype: String, Codable {
-    case empty = ""
-    case photo = "photo"
-}
-
-enum MediaType: String, Codable {
-    case image = "image"
-}
-
-enum Source: String, Codable {
-    case newYorkTimes = "New York Times"
-}
-
-enum ResultType: String, Codable {
-    case article = "Article"
-    case interactive = "Interactive"
 }
 
 // MARK: - Encode/decode helpers
